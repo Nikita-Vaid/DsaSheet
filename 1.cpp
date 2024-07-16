7e20402c46234c4cd711444153d148fd2f22e439
Nikita-Vaid <vaidnikita06@gmail.com>
2024-07-16 22:42:22 +0530

    2ndchange

--- 1.cpp
+++ 1.cpp
@@ -1,4 +1,36 @@
 #include <iostream>
-int main(){
-    
+
+
+double multiply(double number, int n) {
+    double ans = 1.0;
+    for (int i = 1; i <= n; i++) {
+        ans = ans * number;
+    }
+    return ans;
+}
+
+double getNthRoot(int n, int m) 
+{
+    double low = 1;
+    double high = m;
+    double eps = 1e-6;
+    while ((high - low) > eps) 
+    {
+        double mid = (low + high) / 2;
+        if (multiply(mid, n) < m) {
+            low = mid;
+        } else {
+            high = mid;
+        }
+    }
+    return low; // or return (low + high) / 2;
+}
+
+
+int main() {
+    int n = 3;
+    int m = 27;
+    double result = getNthRoot(n, m);
+    std::cout << "The " << n << "th root of " << m << " is " << result << std::endl;
+    return 0;
 }
